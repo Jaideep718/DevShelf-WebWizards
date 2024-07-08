bda05efb1d4735b9f8b089d1a48ea222420994cd
balajiimandi2904 <166785864+balajiimandi2904@users.noreply.github.com>
2024-07-08 20:53:53 +0530

    App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,41 +1,22 @@
-/*import React from 'react';
-import Login from './components/Login';
-import LoginPage from './components/LoginPage';
-import Signup from './components/signUp';
-import { Route, Routes } from "react-router-dom";
-
-function App() {
-  return (
-  <>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/signUp' element={<Signup/>}/>
-    </Routes>
-  </>
-  );
-}
-export default App */
- 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import LoginPage from './components/LoginPage';
 import Signup from './components/signUp';
-import BooksList from './components/BookList';
+import BooksList from './components/BooksList';
 import BookDetail from './components/BookDetail';
 
 function App() {
   return (
-    <Router>
+    <>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signUp' element={<Signup />} />
-        <Route path='/books' element={<BooksList />} />
-        <Route path='/book/:id' element={<BookDetail />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/books" element={<BooksList />} />
+        <Route path="/book/:id" element={<BookDetail />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
