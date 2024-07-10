5a74fe8027f0fbc1fb584805ecdeefcccf808d27
balajiimandi2904 <166785864+balajiimandi2904@users.noreply.github.com>
2024-07-10 20:27:05 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import LoginPage from './components/LoginPage';
 import Signup from './components/signUp';
-// import BookList from './components/BookList';
-import BookDetail from './components/BookDetail';
+import BookDetails from './components/BookDetail';
 import Books from './components/Books';
 
 function App() {
@@ -14,9 +13,8 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/books" element={<BookList />} /> */}
         <Route path="/books" element={<Books />} />
-        <Route path="/book/:id" element={<BookDetail />} />
+        <Route path="/book/:title" element={<BookDetails />} />
       </Routes>
     </>
   );
