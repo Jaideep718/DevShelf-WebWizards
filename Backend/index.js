9b8ac1cb2f20ced7eb328afadc507460bae161f7
Jaideep718 <reddy.jaideep18@gmail.com>
2024-07-13 11:36:39 +0530

    .env file added

--- Backend/index.js
+++ Backend/index.js
@@ -231,7 +231,7 @@ app.post('/return', async (req, res) => {
 
 
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
