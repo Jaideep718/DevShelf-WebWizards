bd5d23cca684ee1afb7791cbaf71097ee3894c37
Jaideep718 <reddy.jaideep18@gmail.com>
2024-07-14 09:39:31 +0530

    axios.js updated

--- Frontend/src/components/axios.js
+++ Frontend/src/components/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://devshelf-webwizards-backend.onrender.com', // Replace with your backend URL
+  // baseURL: 'https://devshelf-webwizards-backend.onrender.com',
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+   // Replace with your backend URL
 });
 
 export default api;
